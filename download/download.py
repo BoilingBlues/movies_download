052bafdf4a12faa3cb5a39daf64eccf818d27046
BoilingBlues <qq22430767@gmail.com>
2020-02-05 18:42:12 +0800

    添加断点下载和进图条显示功能v0.2

--- download/download.py
+++ download/download.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 #coding=utf-8
 
+import os
 import requests
 import sys
+from tqdm import tqdm
 
 class DownLoad():
     ''' 
@@ -14,25 +16,41 @@ class DownLoad():
         
     def download_videofile(self,link):
         '''
-        逐个下载视频
-        参数:视频链接列表
+        参数:视频链接
         '''
+        response = requests.get(link,stream=True)
+        file_size = int(response.headers['content-length'])
+        response.close()
         file_name = link.split('/')[-1]
-        #在这里加命令行进度条
-        print("开始下载:%s" % file_name)
-        #在这里加命令行进度条
-        r = requests.get(link,stream=True)
-        with open(self.root+file_name,'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024*1024):
+        file_dst = self.root+file_name
+        #断点判断
+        if os.path.exists(file_dst):
+            first_byte=os.path.getsize(file_dst)
+        else:
+            first_byte = 0
+        header = {
+            "Range":f"bytes={first_byte}-{file_size}"
+        }
+        #断点请求
+        response = requests.get(link,headers=header,stream=True)
+        
+        #下载文件
+        print("正在下载%s" %file_name)
+        pbar = tqdm(total=file_size,initial=first_byte,unit='B',unit_scale=True,desc="下载进度",miniters=1)
+         ##unit:定义迭代单元,B为字节,默认bit,,,unit_scale:进制转换自动转换MB和KB,,,miniters:最小更新间隔.
+
+        with open(file_dst,'wb') as f:
+            for chunk in response.iter_content(chunk_size=1024):
                 if chunk:
                     f.write(chunk)
-        #在这里加命令行进度条
-        print("%s 下载完成\n" % file_name)
+                    pbar.update(1024)
+        pbar.close()
+        print("下载完成\n")
+        response.close()
         return
 
 
 if  __name__ == "__main__":
-    print("输入文件路径和视频链接")
     download = DownLoad(sys.argv[1])
     for link in sys.argv[2:]:
         download.download_videofile(link)    
